1abe4b171672926ae48fc50b88a09e7a04f0b936
Emanuelle <emanuellemarreira@gmail.com>
2023-12-24 03:14:26 -0400

    Add files via upload

--- testepp2.cpp
+++ testepp2.cpp
@@ -2,41 +2,44 @@
 #include <vector>
 #include <list>
 #include <limits>
+
 using namespace std;
+
 const float inf = std::numeric_limits<float>::infinity();
-
-typedef char Vertex;
-
-template <typename T>
-class No {
-public:
-    T valor;
-    No<T>* proximo;
-
-    No(T v) : valor(v), proximo(nullptr) {}
-};
-
-template <typename T>
-class Fila {
-public:
-    No<T>* inicio;
-    No<T>* fim;
-    Fila() : inicio(nullptr), fim(nullptr) {}
-    void destruirFila();
-    bool enfileirar(T novo_valor);
-    No<T>* desenfileirar();
-    void imprimeFila();
-};
 
 class ItemVertex
 {
 public:
-    Vertex value;
+    char name;
     int d;
-    unsigned int visita;
+    unsigned int color;
     ItemVertex *predecessor;
-    ItemVertex() {}
-    ItemVertex(char value) : value(value){};
+
+    // Construtor padrão
+    ItemVertex() : name('\0'), d(inf), color(0), predecessor(nullptr) {}
+
+    // Construtor com parâmetros
+    ItemVertex(char _name, int _d, unsigned int _color, ItemVertex* _predecessor)
+        : name(_name), d(_d), color(_color), predecessor(_predecessor) {}
+};
+
+template <typename T>
+class Queue {
+private:
+    std::list<T> list;
+
+public:
+    Queue() {}
+    void enqueue(T item) { list.push_back(item); }
+    T dequeue()
+    {
+        if (!list.empty())
+        {
+            T front = list.front();
+            list.pop_front();
+            return front;
+        }
+    }
 };
 
 template <typename T>
@@ -46,70 +49,24 @@ private:
     unsigned int num_vertices;
     unsigned int num_edges;
     list<T> *adj;
+    std::list<ItemVertex> allVertices;  // Lista de todos os ItemVertex adicionados
 
 public:
     GraphAL(unsigned int);
     ~GraphAL();
-    void add_edge(Vertex, Vertex);
-    void remove_edge(Vertex, Vertex);
-    bool vertex_already_exists(Vertex u, Vertex v);
-    list<T> get_adj(Vertex v) { return adj[v]; }
+    void add_edge(char u, char v);
+    void remove_edge(char u, char v);
+    bool vertex_already_exists(char u, char v);
+    list<T> get_adj(char v) { return adj[v - 96]; }
     unsigned int get_num_vertices() { return num_vertices; }
     unsigned int get_num_edges() { return num_edges; }
+    std::list<ItemVertex> get_all_vertices() { return allVertices; }
 };
-
-template <typename T>
-void Fila<T>::destruirFila() {
-        No<T>* apagar;
-        No<T>* i = inicio;
-        while (i != nullptr) {
-            apagar = i;
-            i = i->proximo;
-            delete apagar;
-        }
-}
-
-template <typename T>
-bool Fila<T>::enfileirar(T novo_valor) {
-        No<T>* i = new No<T>(novo_valor);
-        if (i == nullptr) {
-            return false;
-        }
-        i->valor = novo_valor;
-        i->proximo = nullptr;
-        if (inicio == nullptr) {
-            inicio = i;
-        } else {
-            fim->proximo = i;
-        }
-        fim = i;
-        return true;
-    }
-
-template <typename T>
-No<T>* Fila<T>::desenfileirar() {
-        if (inicio == nullptr) {
-            return nullptr;
-        }
-        No<T>* i = inicio;
-        inicio = inicio->proximo;
-        return i;
-    }
-
-template <typename T>
-void Fila<T>::imprimeFila() {
-        No<T>* i = inicio;
-        while (i != nullptr) {
-            std::cout << " " << i->valor << " ";
-            std::cout << std::endl;
-            i = i->proximo;
-        }
-}
 
 template <typename T>
 GraphAL<T>::GraphAL(unsigned int num_vertices) : num_vertices(num_vertices)
 {
-    adj = new list<ItemVertex>[num_vertices];
+    adj = new list<T>[num_vertices];
 }
 
 template <typename T>
@@ -123,56 +80,68 @@ GraphAL<T>::~GraphAL()
     adj = nullptr;
     num_edges = 0;
     num_vertices = 0;
+    allVertices.clear();  // Limpar a lista de todos os ItemVertex
 }
 
 template <typename T>
-void GraphAL<T>::add_edge(Vertex u, Vertex v)
+void GraphAL<T>::add_edge(char u, char v)
 {
-    ItemVertex item_Vertex_v{v};
-    ItemVertex item_Vertex_u{u};
+    ItemVertex item_Vertex_v{v,  inf, 0, nullptr};
+    ItemVertex item_Vertex_u{u,  inf, 0, nullptr};
     if (vertex_already_exists(u, v) == false)
     {
-        adj[u].push_back(item_Vertex_v);
+        adj[u - 96].push_back(item_Vertex_v); //u - 96 pq u vai ser a letra
+        allVertices.push_back(item_Vertex_v);  // Adicionar a v à lista de todos os ItemVertex
     }
     if (vertex_already_exists(v, u) == false)
     {
-        adj[v].push_back(item_Vertex_u);
+        adj[v - 48].push_back(item_Vertex_u);//u - 48 pq u vai ser o numero
+        allVertices.push_back(item_Vertex_u);  // Adicionar a u à lista de todos os ItemVertex
     }
     num_edges++;
 }
 
 template <typename T>
-void GraphAL<T>::remove_edge(Vertex u, Vertex v)
+void GraphAL<T>::remove_edge(char u, char v)
 {
-    ItemVertex item_Vertex_v{v};
-    ItemVertex item_Vertex_u{u};
-    list<ItemVertex> &lista_u = adj[u];
+    list<ItemVertex> &lista_u = adj[u - 96];
     for (auto itr = lista_u.begin(); itr != lista_u.end(); ++itr)
     {
-        if (itr->value == item_Vertex_v.value)
+        if (itr->name == v)
         {
             itr = lista_u.erase(itr);
+            break;
         }
     }
-    list<ItemVertex> &lista_v = adj[v];
+
+    list<ItemVertex> &lista_v = adj[v - 48];
     for (auto itr2 = lista_v.begin(); itr2 != lista_v.end(); ++itr2)
     {
-        if (itr2->value == item_Vertex_u.value)
+        if (itr2->name == u)
         {
             itr2 = lista_v.erase(itr2);
+            break;
         }
     }
-    num_edges--;
+
+    // Remover da lista de todos os ItemVertex ==================ver isso aqui dps pq tem algo dando errado
+    /*auto itrAll = std::find_if(allVertices.begin(), allVertices.end(),
+                               [&](const ItemVertex &item) { return item.name == u || item.name == v; });
+    if (itrAll != allVertices.end())
+    {
+        allVertices.erase(itrAll);
+    }
+
+    num_edges--;*/
 }
+
 template <typename T>
-bool GraphAL<T>::vertex_already_exists(Vertex u, Vertex v)
+bool GraphAL<T>::vertex_already_exists(char u, char v)
 {
-    ItemVertex item_Vertex_u{u};
-    ItemVertex item_Vertex_v{v};
-    list<ItemVertex> &lista_u = adj[u];
+    list<ItemVertex> &lista_u = adj[u - 96];
     for (auto itr = lista_u.begin(); itr != lista_u.end(); ++itr)
     {
-        if (itr->value == item_Vertex_v.value)
+        if (itr->name == v)
         {
             return true;
         }
@@ -183,8 +152,7 @@ bool GraphAL<T>::vertex_already_exists(Vertex u, Vertex v)
 template <typename T>
 void input_GraphAL(GraphAL<T> &g, unsigned int num_edges)
 {
-    Vertex u = 0;
-    Vertex v = 0;
+    char u, v;
     for (unsigned int i = 0; i < num_edges; ++i)
     {
         cin >> u >> v;
@@ -197,7 +165,7 @@ void display_list(list<T> &lst)
 {
     for (auto item_Vertex : lst)
     {
-        cout << " (" << item_Vertex.value << ", "<< item_Vertex.d << ", "<< item_Vertex.visita << ") ";
+        cout << " (" << item_Vertex.name << ", " << item_Vertex.d << ", " << item_Vertex.color << ") ";
     }
     cout << endl;
 }
@@ -205,52 +173,13 @@ void display_list(list<T> &lst)
 template <typename T>
 void display_GraphAL(GraphAL<T> &g)
 {
-    for (unsigned int v = 0; v < g.get_num_vertices(); v++)
-    {
-        cout << v << ": ";
-        list<T> lst = g.get_adj(v);
+    list<ItemVertex> allVertices = g.get_all_vertices();
+    for (auto vertex : allVertices){
+        cout << vertex.name << ": ";
+        list<T> lst = g.get_adj(vertex.name);
         display_list(lst);
     }
-}
 
-template <typename T>
-void BFS(GraphAL<T> &g, Vertex s){
-    //percorre cada vertice de g
-    ItemVertex item_Vertex_s{s};
-    for (unsigned int v = 0; v < g.get_num_vertices(); v++)
-    {
-        list<T> lst = g.get_adj(v);
-        for (auto item_Vertex : lst)
-        {
-            if(item_Vertex.value != item_Vertex_s.value){
-                item_Vertex.visita = 0;
-                item_Vertex.d = inf;
-                item_Vertex.predecessor = nullptr;
-            }
-        }
-    }
-    item_Vertex_s.visita = 1;
-    item_Vertex_s.d = 0;
-    item_Vertex_s.predecessor = nullptr;
-    Fila<T> fila;
-    fila.enfileirar(item_Vertex_s);
-    ItemVertex item_Vertex_u{};
-    while (fila.inicio != nullptr){
-        No<T>* teste = fila.desenfileirar();
-        item_Vertex_u = teste->valor; 
-        delete teste;
-        list<T> adju = g.get_adj(item_Vertex_u.value); //pq value é vertex
-        for (auto v : adju)
-        {
-            if(v.visita == 0){
-                v.visita = 1;
-                v.d = item_Vertex_u.d + 1;
-                v.predecessor = &item_Vertex_u;
-                fila.enfileirar(v);
-            }
-        }
-        item_Vertex_u.visita = 2;
-    }
 }
 
 int main()
@@ -262,8 +191,7 @@ int main()
     cout << "num_edges: " << num_edges << endl;
     GraphAL<ItemVertex> g{num_vertices};
     input_GraphAL(g, num_edges);
-    Vertex s;
-    BFS(g, s);
+
     display_GraphAL(g);
     return 0;
 }
